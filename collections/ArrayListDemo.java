39657cb5256c6b48f7bf7f17ceaa4f9ee12acdb8
kusuma447 <uppalapatikusuma74@gmail.com>
2026-06-18 18:34:38 +0530

    day_01 ArrayList

---
+++ collections/ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class ArrayListDemo{
+    public static void main(String[] args){
+        ArrayList <Integer> list =new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        System.out.println(list.get(3));
+        System.out.println(list.set(1,60));
+        System.out.println(list.contains(30));
+        System.out.println(list.size());
+        System.out.println(list.remove(2));
+    }
+}
