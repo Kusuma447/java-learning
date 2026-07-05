9ac515828c74fd4f9d62059e14cd7fb60f61d530
kusuma447 <uppalapatikusuma74@gmail.com>
2026-07-05 20:19:30 +0530

    overriding both equals and hashcode in HashMAp

---
+++ collections/StudentTest.java
@@ -0,0 +1,30 @@
+
+/*overriding both equals and hashcode for correct implemetation of having  same details.
+*/
+import java.util.HashMap;
+ class Student{
+    int rollNo;
+    String name;
+    Student(int rollNo,String name){
+        this.rollNo=rollNo;
+        this.name=name;
+    }
+    public boolean equals(Object obj){
+        Student other=(Student)obj;
+        return this.rollNo==other.rollNo;
+    }
+    public int hashCode(){
+        return rollNo;
+    }
+}
+public class StudentTest{
+    public static void main(String[] args){
+        HashMap<Student,String>map=new HashMap<>();
+        Student s1=new Student(101,"j");
+        Student s2=new Student(101,"j");
+        map.put(s1,"CSE");
+        System.out.println(map.get(s2));
+        s2.equals(s1);
+        s2.hashCode();
+    }
+}
