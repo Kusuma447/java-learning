3454480796bc566914429cab1b4e28d8bcf90c7b
kusuma447 <uppalapatikusuma74@gmail.com>
2026-07-13 06:16:56 +0530

    Predicate interface

---
+++ Java 8/PredicateDemo.java
@@ -0,0 +1,7 @@
+import java.util.function.Predicate;
+public class PredicateDemo {
+    public static void main(String[] args){
+        Predicate<Integer>p=x->x>10;
+        System.out.println(p.test(15));
+    }
+}
